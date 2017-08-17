4597232713882dab30affac62e9cd4dd8520159f
Mike Baynton <mbaynton@umn.edu>
2017-08-17 13:25:09 -0500

    Try a different way of setting the global php version.

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -6,13 +6,8 @@
 #
 # Document parameters here.
 #
-class profile::php (
-  $php_version = '7.1.7'
-) {
-  class { '::php::globals':
-    php_version => $php_version
-  }
-  -> class { '::php':
+class profile::php {
+	class { '::php':
     manage_repos => true, # puppet-php currently uses remirepo.net for CentOS
   }
 }
