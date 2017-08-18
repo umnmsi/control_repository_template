fa57affe09e93e2a72cc499a2aa187014f6b0765
Mike Baynton <mbaynton@umn.edu>
2017-08-18 08:31:47 -0500

    Try adding the php71 repo ourselves, sadly voxpopuli/php does not really have the repo management together.

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -7,13 +7,18 @@
 # Document parameters here.
 #
 class profile::php {
-  class { '::php::repo::redhat':
-    yum_repo => 'remi_php71',
-  }
-  -> class { '::php::globals':
+  class { '::php::globals':
     php_version => '7.1',
   }
+  -> yumrepo { 'remi-php71':
+    descr      => "Remi\'s PHP 7.1 RPM repository for Enterprise Linux ${::releasever} - ${::basearch}",
+    mirrorlist => "https://rpms.remirepo.net/enterprise/${::releasever}/php71/mirror",
+    enabled    => 1,
+    gpgcheck   => 1,
+    gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+    priority   => 1,
+  }
   -> class { '::php':
-    manage_repos => true,
+    manage_repos => false,
   }
 }
