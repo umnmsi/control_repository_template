062c06ef6b42036b99e208ff47a60899facc5604
Mike Baynton <mbaynton@umn.edu>
2017-08-31 10:35:04 -0500

    Refactor for multiple vhosts, add cilogon configuration

--- site/profile/manifests/nextcloud_server.pp
+++ site/profile/manifests/nextcloud_server.pp
@@ -4,112 +4,131 @@
 #
 # === Parameters
 #
-# [*fqdn*]
-#   The fully qualified domain name of this NextCloud.
-# [*ip*]
-#   The IP address where this NextCloud will be hosted.
+# [*sites*]
+#   An array of hashes containing fqdn, ssl key, and other settings. Each array element creates a NextCloud-ready vhost.
 #
 class profile::nextcloud_server (
-  String $fqdn = $trusted['certname'],
-  String $ip   = $facts['ipaddress'],
+  Array $sites,
 ) {
-  ############################
-  ### Apache configuration ###
-  ############################
+  $ssl_certs_dir = $::apache::params::ssl_certs_dir
+  $apache_conf_dir = $::apache::params::conf_dir
+
   include profile::apache_webserver
+  include profile::mysql
 
   # This breaks role/profile rules by including profile::php with resource-like
-  # syntax, but short of duplicating lots of configuraiton in each nodes' hiera
+  # syntax, but short of duplicating lots of configuration in each nodes' hiera
   # settings, there doesn't seem to be an alternative to apply nextcloud-specific
   # php config.
   class { 'profile::php':
     ini_settings => {
-      'PHP/upload_max_filesize' => '32M',
-      'PHP/post_max_size'       => '32M',
+      'PHP/upload_max_filesize' => '128M',
+      'PHP/post_max_size'       => '128M',
       'PHP/max_input_time'      => '300',
-      'PHP/memory_limit'        => '64M',
+      'PHP/memory_limit'        => '512M',
       'Date/date.timezone'      => 'America/Chicago',
     }
   }
 
-  # If we wanted to run multiple sites per physical node, resources below
-  # here could probably be added to a lambda over an array of hashes providing
-  # fqdn and running user/group. We'd mostly just want to add a php::fpm::pool
-  # resource for each.
-
-  $docroot = "/var/www/${fqdn}"
-  $ssl_certs_dir = $::apache::params::ssl_certs_dir
-
-  file { "${ssl_certs_dir}/${fqdn}.crt":
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => "puppet:///modules/ssl_certs/${fqdn}.crt",
-    notify => Service['httpd'],
+  $ip_based = length($sites) ? {
+    1       => true,
+    default => false,
   }
-  $website_ssl_keys = lookup('website_ssl_keys')
-  file { "${ssl_certs_dir}/${fqdn}.key":
-    owner     => 'root',
-    group     => 'root',
-    mode      => '0640',
-    content   => $website_ssl_keys[$fqdn],
-    show_diff => false,
-    notify    => Service['httpd'],
+  $ip = $ip_based ? {
+    true    => $facts['ipaddress'],
+    default => undef,
   }
 
-  php::fpm::pool { $fqdn:
-    listen => '127.0.0.1:9000',
-    user   => $::apache::user,
-    group  => $::apache::group,
-  }
+  $sites.each |Hash $site, Bool $ip_based, String $ip| {
+    ############################
+    ### Apache configuration ###
+    ############################
+    $fqdn = $site['fqdn']
+    $docroot = "/var/www/${fqdn}"
 
-  apache::vhost { $fqdn:
-    servername       => $fqdn,
-    ssl              => true,
-    port             => 443,
-    ip_based         => true,
-    ip               => $ip,
-    ssl_cert         => "${ssl_certs_dir}/${fqdn}.crt",
-    ssl_key          => "${ssl_certs_dir}/${fqdn}.key",
-    docroot          => $docroot,
-    directories      => [
-      {
-        path             => $docroot,
-        headers          => 'always set Strict-Transport-Security "max-age=15552000; includeSubDomains"',
-        'options'        => ['+FollowSymLinks'],
-        'allow_override' => ['All'],
-      }
-    ],
-    proxy_pass_match => [
-      { 'path' => '^/(.*\.php(/.*)?)$', 'url' => "fcgi://127.0.0.1:9000${docroot}/" }
-    ],
-    setenv           => [
-      "HOME ${docroot}",
-      "HTTP_HOME ${docroot}",
-    ],
-    block            => ['scm'],
-  }
+    file { "${ssl_certs_dir}/${fqdn}.crt":
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet:///modules/ssl_certs/${fqdn}.crt",
+      notify => Service['httpd'],
+    }
+    file { "${ssl_certs_dir}/${fqdn}.key":
+      owner     => 'root',
+      group     => 'root',
+      mode      => '0640',
+      content   => $site['ssl_key'],
+      show_diff => false,
+      notify    => Service['httpd'],
+    }
 
-  apache::vhost { "${fqdn}-http":
-    servername      => $fqdn,
-    ssl             => false,
-    port            => 80,
-    ip_based        => true,
-    ip              => $ip,
-    docroot         => '/var/www/nonssl_redirect_empty_docroot',
-    redirect_status => 'permanent',
-    redirect_dest   => "https://${fqdn}/",
-  }
+    $cilogon_config = "${apache_conf_dir}/${fqdn}_cilogon.conf"
+    $oidc_client_id = $site['cilogon_client_id']
+    $oidc_client_secret = $site['cilogon_client_secret']
+    $oidc_crypto_passphrase = lookup('OIDCCryptoPassphrase')
 
-  ##############################
-  ### Database Configuration ###
-  ##############################
-  include profile::mysql
+    file { $cilogon_config:
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => template('website/nextcloud-cilogon.conf.erb'),
+      notify  => Service['httpd'],
+    }
 
-  mysql::db { 'nextcloud':
-    user     => 'nextcloud',
-    password => '*33D8B521314C648A6D5125CF472C3F43C317B339', # This is a hash
-    host     => 'localhost',
-    grant    => ['ALL']
+    $fpm_port = $site['php_fpm_port']
+    php::fpm::pool { $fqdn:
+      listen => "127.0.0.1:${fpm_port}",
+      user   => $site['php_fpm_user'],
+      group  => $site['php_fpm_group'],
+    }
+
+    apache::vhost { $fqdn:
+      servername          => $fqdn,
+      ssl                 => true,
+      port                => 443,
+      ip_based            => $ip_based,
+      ip                  => $ip,
+      ssl_cert            => "${ssl_certs_dir}/${fqdn}.crt",
+      ssl_key             => "${ssl_certs_dir}/${fqdn}.key",
+      docroot             => $docroot,
+      directories         => [
+        {
+          path             => $docroot,
+          headers          => 'always set Strict-Transport-Security "max-age=15552000; includeSubDomains"',
+          'options'        => ['+FollowSymLinks'],
+          'allow_override' => ['All'],
+        }
+      ],
+      proxy_pass_match    => [
+        { 'path' => '^/(.*\.php(/.*)?)$', 'url' => "fcgi://127.0.0.1:${fpm_port}${docroot}/" }
+      ],
+      setenv              => [
+        "HOME ${docroot}",
+        "HTTP_HOME ${docroot}",
+      ],
+      block               => ['scm'],
+      additional_includes => [$cilogon_config]
+    }
+
+    apache::vhost { "${fqdn}-http":
+      servername      => $fqdn,
+      ssl             => false,
+      port            => 80,
+      ip_based        => true,
+      ip              => $ip,
+      docroot         => '/var/www/nonssl_redirect_empty_docroot',
+      redirect_status => 'permanent',
+      redirect_dest   => "https://${fqdn}/",
+    }
+
+    ##############################
+    ### Database Configuration ###
+    ##############################
+    mysql::db { $site['database_name']:
+      user     => $site['database_name'],
+      password => $site['database_password'],
+      host     => 'localhost',
+      grant    => ['ALL']
+    }
   }
 }
