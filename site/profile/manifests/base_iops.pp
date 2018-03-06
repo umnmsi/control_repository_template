deb1bb660ed4e3888d3d350f80bc43b6c3f33111
Mike Baynton <mbaynton@umn.edu>
2018-03-06 12:12:39 -0600

    Manage some default repositories.

--- site/profile/manifests/base_iops.pp
+++ site/profile/manifests/base_iops.pp
@@ -1,3 +1,16 @@
 class profile::base_iops {
+  # Default repositories. Computed based on information we already have
+  # rather than reading from hiera per best practice.
+  case $facts['os']['name'] {
+    'CentOS': {
+      $default_repos = ['base', 'update', 'epel']
+    }
+    default: {
+      $default_repos = []
+    }
+  }
 
+  $default_repos.each |$repo| {
+    pax::repo($repo)
+  }
 }
